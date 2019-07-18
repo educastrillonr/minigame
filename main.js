f599d664fe9ec5b29e5c1cec8cf4d319392eb982
Eduardo Romero <eduardo.romero@Nology.local>
2019-07-18 14:40:48 +0100

    change inputs into buttons with value

--- main.js
+++ main.js
@@ -1,45 +1,104 @@
 let difficulty;
-let round;
+let solution = [];
+let playerInput = [];
+const colorPads = [
+  $("#pad-top-left"),
+  $("#pad-top-right"),
+  $("#pad-bottom-left"),
+  $("#pad-bottom-right")
+];
 
 const goToGame = () => {
-    $('#difficulty-section').hide();
-    $('main').show();
-}
+  $("#difficulty-section").hide();
+  $("main").show();
+  $("input").prop("disabled", true);
+};
 
 const goToDifficultySection = () => {
-    $('main').hide();
-    $('#difficulty-section').show();
-}
+  $("main").hide();
+  $("#difficulty-section").show();
+  $("input").prop("disabled", false);
+};
 
-const setEasy = () => {
-    difficulty = 0;
-    goToGame();
-}
-
-const setMedium = () => {
-    difficulty = 1;
-    goToGame();
-}
-
-const setHard = () => {
-    difficulty = 2;
-    goToGame();
-}
-
-const setGodmode = () => {
-    difficulty = 3;
-    goToGame();
-}
+const setDifficulty = () => {
+  switch ($("input[name='difficulty']").val()) {
+    case "easy":
+      difficulty = 0;
+      break;
+    case "medium":
+      difficulty = 1;
+      break;
+    case "hard":
+      difficulty = 2;
+      break;
+    case "godmode":
+      difficulty = 3;
+      break;
+  }
+  console.log(difficulty);
+  
+  goToGame();
+};
 
 const newGame = () => {
-    $('#start-button').hide();
-    round = 1;
-    $('#gamepad-wrapper > p').html('Round: ' + round);
-    // result.innerHTML = "&#8203";
-    // round = 1;
-    // roundPrint.innerHTML = "Round : " + round;
-    // generateSequence();
-    // displaySequence();
-  };
+  $("#start-button").hide();
+  addRound();
+  $("#gamepad-wrapper > p").html("Round: " + solution.length);
+  displaySequence();
+  enablePlayerInput();
+};
 
-$('main').hide();
+const addRound = () => {
+  solution.push(Math.floor(Math.random() * 4));
+};
+
+const displaySequence = () => {
+  let i = 0;
+  const interval = setInterval(() => {
+    if (i === solution.length) {
+      clearInterval(interval);
+    } else {
+      flashColor(solution[i]);
+      i++;
+    }
+  }, 1000);
+};
+
+const flashColor = position => {
+  const originalColor = colorPads[position].css("background");
+  colorPads[position].css("background", "white");
+  setTimeout(() => {
+    colorPads[position].css("background", originalColor);
+  }, 200);
+};
+
+const enablePlayerInput = () => {
+  $("input[name='game-pads']").prop("disabled", false);
+};
+
+const getPlayerInput = () => {
+  playerInput.push($("input[name='game-pads").val());
+  console.log(playerInput);
+  
+  check();
+};
+
+const check = () => {
+  let correct =
+    playerInput[playerInput.length - 1] == solution[playerInput.length - 1];
+
+  if (!correct) {
+    alert('yu lost');
+    resetGame();
+  }
+};
+
+const resetGame = () => {
+    solution = [];
+    playerInput = [];
+    difficulty = null;
+    $("#start-button").show();
+    $("#gamepad-wrapper > p").html("");
+}
+
+$("main").hide();
